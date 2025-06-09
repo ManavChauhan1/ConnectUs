1b0cab9f2db68e9a38d5ab14d1c315d2ab162f79
Manav Chauhan <92674597+ManavChauhan1@users.noreply.github.com>
2025-06-09 12:14:12 +0530

    Middlewares updated

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 // const upload = require('./config/multerconfig');
 const upload = require('./config/multerconfig');
 
+//Middlewares
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(express.json());
